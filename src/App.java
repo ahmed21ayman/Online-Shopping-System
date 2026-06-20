c26fb34439fd7ea04ed6a9c8a43082c0d225ead2
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-20 18:36:37 +0300

    v1: build parent class product

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
 public class App {
+
+    static class Product{
+        protected String productName;
+        protected double price;
+        protected int quantity;
+        Product(String productName, double price, int quantity){
+            this.productName = productName;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
