da197a1e057b6eeb3bc2a4c91e7eb4880e65d7c2
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-20 18:38:33 +0300

    v2: build vhaild class customer

--- src/App.java
+++ src/App.java
@@ -11,6 +11,17 @@ public class App {
         }
     }
 
+    static class Customer extends Product{
+        private String name;
+        private String email;
+        Customer(String productName, double price, int quantity, String name, String email){
+            super(productName, price, quantity);
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
