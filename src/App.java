76540c6c416fa80c90054514a4f0b200fef9d0e0
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-20 19:39:40 +0300

    v3: add chaild order

--- src/App.java
+++ src/App.java
@@ -21,6 +21,21 @@ public class App {
         }
     }
 
+    static class Order extends Product{
+        private int orderId;
+        private String customer;
+        private String product;
+        Order(String productName, double price, int quantity, int orderId, String customer, String product){
+            super(productName, price, quantity);
+            this.orderId = orderId;
+            this.customer = customer;
+            this.product = product;
+        }
+        void totalPrice(){
+            System.out.println("hello: " + customer + " you order: " + orderId + "\nproduct: " + product + " * " + quantity + "\ntotal price" + price*quantity);
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
